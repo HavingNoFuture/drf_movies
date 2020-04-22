5be4e5aebcbd6eba5b47f96decbd47368e03e474
Alexey Ermilov <alex.erm@yandex.ru>
2020-04-22 14:54:56 +0300

    add dynamic drf schema with swagger ui

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
     path('movies/', views.MovieListView.as_view()),
     path('movies/<int:pk>', views.MovieDetailView.as_view()),
     path('review/', views.ReviewCreateView.as_view()),
-    path('rating/', views.RatingCreateView.as_view())
+    path('rating/', views.RatingCreateView.as_view()),
+    path('openapi', get_schema_view(
+        title="drf movies",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from rest_framework.fields import ReadOnlyField
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,12 +7,16 @@ from .models import Movie
 from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, RatingCreateSerializer
 from .services import get_client_ip_from_request
 
+User = get_user_model()
+
+
 class MovieListView(APIView):
     """Вывод списка фильмов"""
 
     def get(self, request):
         movies = Movie.objects.filter(draft=False)
         serializer = MovieListSerializer(movies, many=True)
+        print(Movie.objects.filter(category__name='movies'))
         return Response(serializer.data)
 
 
@@ -42,3 +48,6 @@ class RatingCreateView(APIView):
             serializer.save(ip=get_client_ip_from_request(request))
             return Response(status=201)
         return Response(status=400)
+
+
+from django.contrib.auth import get_user_model
