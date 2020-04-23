1beb3d5a45fe251b73bdfe53a0616ae61330aac3
Alexey Ermilov <alex.erm@yandex.ru>
2020-04-23 17:23:19 +0300

    rewrite views to generic views

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,6 @@
 from django.db import models
 from rest_framework import generics
 
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 from .models import Movie, Person
 from .serializers import (
     MovieListSerializer,
@@ -16,58 +13,45 @@ from .serializers import (
 from .services import get_client_ip_from_request
 
 
-class MovieListView(APIView):
+class MovieListView(generics.ListAPIView):
     """Вывод списка фильмов"""
+    serializer_class = MovieListSerializer
 
-    def get(self, request):
-        movies = Movie.objects.filter(draft=False).annotate(
-            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip_from_request(request)))
+    def get_queryset(self):
+        queryset = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip_from_request(self.request)))
         ).annotate(
             average_rating=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
         )
-        serializer = MovieListSerializer(movies, many=True)
-        return Response(serializer.data)
+        return queryset
 
 
-class MovieDetailView(APIView):
+class MovieDetailView(generics.RetrieveAPIView):
     """Вывод подробностей фильмов"""
-
-    def get(self, request, pk):
-        movie = Movie.objects.get(id=pk, draft=False)
-        serializer = MovieDetailSerializer(movie)
-        return Response(serializer.data)
+    queryset = Movie.objects.filter(draft=False)
+    serializer_class = MovieDetailSerializer
 
 
 class PersonListView(generics.ListAPIView):
     """Вывод списка персоналий"""
-
     queryset = Person.objects.all()
     serializer_class = PersonListSerializer
 
 
 class PersonDetailView(generics.RetrieveAPIView):
     """Вывод деталей персоналия"""
-
     queryset = Person.objects.all()
     serializer_class = PersonDetailSerializer
 
 
-class ReviewCreateView(APIView):
+class ReviewCreateView(generics.CreateAPIView):
     """Вывод отзывов к фильму"""
-
-    def post(self, request):
-        review = ReviewCreateSerializer(data=request.data)
-        if review.is_valid():
-            review.save()
-        return Response(status=201)
+    serializer_class = ReviewCreateSerializer
 
 
-class RatingCreateView(APIView):
+class RatingCreateView(generics.CreateAPIView):
     """Добавление рейтинга к фильму."""
+    serializer_class = RatingCreateSerializer
 
-    def post(self, request):
-        serializer = RatingCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(ip=get_client_ip_from_request(request))
-            return Response(status=201)
-        return Response(status=400)
+    def perform_create(self, serializer):
+        serializer.save(ip=get_client_ip_from_request(self.request))
